8c2bcacbfbb12c9f40a5c810294085751b8ae60c
Joshua Gato <joshuagato37@gmail.com>
2024-12-08 18:21:39 +0000

    Added the ArrayList class

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import arrays.Array;
 
@@ -13,14 +14,32 @@ public class Main {
 //        System.out.println(numbers.length);
 //        System.out.println(Arrays.toString(numbers));
 
-        Array numbers = new Array(3);
-        numbers.insert(10);
-        numbers.insert(20);
-        numbers.insert(30);
-        numbers.insert(40);
-        numbers.removeAt(1);
-        System.out.println("Index of {10}: " + numbers.indexOf(10));
-        System.out.println("Index of {100}: " + numbers.indexOf(100));
-        numbers.print();
+        // Our Array class
+//        Array numbers = new Array(3);
+//        numbers.insert(10);
+//        numbers.insert(20);
+//        numbers.insert(30);
+//        numbers.insert(40);
+//        numbers.removeAt(1);
+//        System.out.println("Index of {10}: " + numbers.indexOf(10));
+//        System.out.println("Index of {100}: " + numbers.indexOf(100));
+//        numbers.print();
+
+
+        // Java's inbuilt ArrayList
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        System.out.println(list.size());
+        list.remove(3);
+        list.removeFirst();
+        System.out.println(list.indexOf(20));
+        System.out.println(list.lastIndexOf(30));
+        System.out.println(list.contains(30));
+        System.out.println(Arrays.toString(list.toArray()));
+
+        System.out.println(list);
     }
 }
