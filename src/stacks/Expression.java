e329f1bc49691cf205b652ad5343946a9648ed02
Joshua Gato <joshuagato37@gmail.com>
2024-12-12 15:50:45 +0000

    First refactoring done

--- src/stacks/Expression.java
+++ src/stacks/Expression.java
@@ -7,21 +7,31 @@ public class Expression {
         Stack<Character> stack = new Stack<>();
 
         for (char ch : input.toCharArray()) {
-            if (ch == '(' || ch == '<' || ch == '[' || ch == '{') stack.push(ch);
+            if (isLeftBracket(ch)) stack.push(ch);
 
-            if (ch == ')' || ch == '>' || ch == ']' || ch == '}') {
+            if (isRightBracket(ch)) {
                 if (stack.isEmpty()) return false;
                 var top = stack.pop();
 
-                if (
-                        (ch == ')' && top != '(') ||
-                        (ch == '>' && top != '<') ||
-                        (ch == ']' && top != '[') ||
-                        (ch == '}' && top != '{')
-                ) return false;
+                if (bracketsDontMatch(top, ch)) return false;
             }
         }
 
         return stack.isEmpty();
+    }
+
+    private boolean isLeftBracket(char ch) {
+        return ch == '(' || ch == '<' || ch == '[' || ch == '{';
+    }
+
+    private boolean isRightBracket(char ch) {
+        return ch == ')' || ch == '>' || ch == ']' || ch == '}';
+    }
+
+    private boolean bracketsDontMatch(char left, char right) {
+        return (right == ')' && left != '(') ||
+                (right == '>' && left != '<') ||
+                (right == ']' && left != '[') ||
+                (right == '}' && left != '{');
     }
 }
