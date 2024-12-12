0ca3596b39bce93d9853819177fded8472d35a3e
Joshua Gato <joshuagato37@gmail.com>
2024-12-12 16:19:01 +0000

    Second refactoring done

--- src/stacks/Expression.java
+++ src/stacks/Expression.java
@@ -1,8 +1,14 @@
 package stacks;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
 public class Expression {
+
+    private final List<Character> leftBrackets = Arrays.asList('(', '<', '[', '{');
+    private final List<Character> rightBrackets = Arrays.asList(')', '>', ']', '}');
+
     public boolean isBalanced(String input) {
         Stack<Character> stack = new Stack<>();
 
@@ -13,7 +19,8 @@ public class Expression {
                 if (stack.isEmpty()) return false;
                 var top = stack.pop();
 
-                if (bracketsDontMatch(top, ch)) return false;
+//                if (bracketsDontMatch(top, ch)) return false;
+                if (!bracketsMatch(top, ch)) return false;
             }
         }
 
@@ -21,10 +28,22 @@ public class Expression {
     }
 
     private boolean isLeftBracket(char ch) {
-        return ch == '(' || ch == '<' || ch == '[' || ch == '{';
+        return leftBrackets.contains(ch);
     }
 
     private boolean isRightBracket(char ch) {
+        return rightBrackets.contains(ch);
+    }
+
+    private boolean bracketsMatch(char left, char right) {
+        return leftBrackets.indexOf(left) == rightBrackets.indexOf(right);
+    }
+
+    private boolean isLeftBracket__(char ch) {
+        return ch == '(' || ch == '<' || ch == '[' || ch == '{';
+    }
+
+    private boolean isRightBracket__(char ch) {
         return ch == ')' || ch == '>' || ch == ']' || ch == '}';
     }
 
